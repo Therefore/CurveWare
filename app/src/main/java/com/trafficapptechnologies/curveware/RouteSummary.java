5a0d82c997d1d3f9b76855965a319efd36c5270e
Therefore <nateokane@gmail.com>
2019-01-16 22:24:44 -0500

    Adding a bunch of stuff

--- app/src/main/java/com/trafficapptechnologies/curveware/RouteSummary.java
+++ app/src/main/java/com/trafficapptechnologies/curveware/RouteSummary.java
@@ -1,18 +1,28 @@
 package com.trafficapptechnologies.curveware;
 
+import android.content.Intent;
 import android.icu.text.LocaleDisplayNames;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class RouteSummary extends AppCompatActivity {
+
+    //UI Elements
+    Button sendButton;
 
     //UI elements
     TextView distanceTextView, averageSpeedTextView, durationTextView, curveynessFactor;
@@ -26,10 +36,34 @@ public class RouteSummary extends AppCompatActivity {
         averageSpeedTextView = findViewById(R.id.averageSpeedTV);
         durationTextView = findViewById(R.id.durationTV);
         curveynessFactor = findViewById(R.id.curveynessFactorTV);
-
         distanceTextView.setText(distanceFormat(Datapoint.getDistance()));
-
         durationTextView.setText(Long.toString(Datapoint.getDuration()));
+        sendButton = findViewById(R.id.sendBTN);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //This actually attaches the file that was created. It can be shared to any application using the switcher.
+                Toast.makeText(RouteSummary.this, "Test Button Pressed", Toast.LENGTH_SHORT).show();
+                //generateNoteOnSD(RunningRoute.this,"emailfile","this is the body");
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/plain");
+                //emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"email@example.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject here");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "body text");
+                File root = new File(Environment.getExternalStorageDirectory(), "Curveware Files");
+                String pathToMyAttachedFile = "emailfile";
+                File file = new File(root, pathToMyAttachedFile);
+                Log.d("path",file.toString());
+                if (!file.exists() || !file.canRead()) {
+                    return;
+                }
+                Uri uri = Uri.fromFile(file);
+                emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
+            }
+        });
 
 
 
